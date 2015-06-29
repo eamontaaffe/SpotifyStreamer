6cf659df9613fbcd485ec9734bb55c88fc14eb84
Eamon Taaffe <eamontaaffe@gmail.com>
2015-06-29 17:54:00 +1000

    search artist is functional. only returns boring listview

--- app/src/main/java/au/com/taaffe/spotifystreamer/SearchFragment.java
+++ app/src/main/java/au/com/taaffe/spotifystreamer/SearchFragment.java
@@ -1,5 +1,6 @@
 package au.com.taaffe.spotifystreamer;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,19 +9,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class SearchFragment extends Fragment {
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> artistAdapter;
 
     private final String LOG_TAG = SearchFragment.class.getSimpleName();
+    FetchArtistTask fetchArtistTask = new FetchArtistTask();
 
     public SearchFragment() {
     }
@@ -30,8 +39,22 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        Log.v(LOG_TAG, "OnCreate");
-        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        SearchView artistSearch = (SearchView) rootView.findViewById(R.id.artist_search_view);
+        artistSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.v(LOG_TAG, "OnQuerySubmit :" + query);
+                updateArtistList(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                return false;
+            }
+        });
+
+        // Create some dummy data for vthe ListView.  Here's a sample weekly forecast
         String[] data = {
                 "Coldplay",
                 "Coldplay & Lele",
@@ -45,19 +68,70 @@ public class SearchFragment extends Fragment {
         // The ArrayAdapter will take data from a source (like our dummy forecast)
         // use it to populate the ListView it's attached to.
 
-        adapter = new ArrayAdapter<String>(
+        artistAdapter = new ArrayAdapter<String>(
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_artist, // The name of the layout ID.
                         R.id.list_item_forecast_textview,
                         artists
         );
 
-        Log.v(LOG_TAG,adapter.toString());
+        Log.v(LOG_TAG, artistAdapter.toString());
 
-        // Get a reference to the ListView, and attach this adapter to it.
+        // Get a reference to the ListView, and attach this artistAdapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview);
-        listView.setAdapter(adapter);
+        listView.setAdapter(artistAdapter);
 
         return rootView;
     }
+
+    void updateArtistList(String query) {
+        Log.v(LOG_TAG, "updateArtistList: " + query);
+        fetchArtistTask.execute(query);
+    }
+
+    public class FetchArtistTask extends AsyncTask<String, Void, Void> {
+        private final String LOG_TAG = FetchArtistTask.class.getSimpleName();
+        ArtistsPager results;
+
+        @Override
+        protected Void doInBackground(String... params) {
+            if (params.length == 0) {
+                Log.v(LOG_TAG,"Empty params list");
+                return null;
+            }
+
+            String query = params[0];
+
+            Log.v(LOG_TAG, "doInBackground, query: " + params[0]);
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService spotify = api.getService();
+            results = spotify.searchArtists(query);
+
+            Log.v(LOG_TAG, "Search results are size: " + results.artists.items.size());
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            int resultsSize = results.artists.items.size();
+            if (resultsSize == 0) {
+                Toast toast = Toast.makeText(getActivity(),
+                        "There are " + resultsSize + " results",
+                        Toast.LENGTH_SHORT
+                );
+                toast.show();
+            }
+
+            // update artistAdapter
+            artistAdapter.clear();
+            for(Artist result: results.artists.items) {
+                artistAdapter.add(result.name);
+            }
+
+        }
+    }
+
 }
