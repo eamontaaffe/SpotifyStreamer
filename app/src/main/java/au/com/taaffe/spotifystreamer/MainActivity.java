9cd5b271cd40da143554fe4c08286586872803f7
Eamon Taaffe <eamontaaffe@gmail.com>
2015-07-01 13:32:38 +1000

    implement back button

--- app/src/main/java/au/com/taaffe/spotifystreamer/MainActivity.java
+++ app/src/main/java/au/com/taaffe/spotifystreamer/MainActivity.java
@@ -1,5 +1,6 @@
 package au.com.taaffe.spotifystreamer;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,17 +24,13 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
