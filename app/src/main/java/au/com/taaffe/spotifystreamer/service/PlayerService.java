6b7027ec44197147dfafb67dd5057933fac2a0e1
Eamon Taaffe <eamontaaffe@gmail.com>
2015-08-04 14:46:07 +1000

    got the notifications working just need to implement the media player again

--- app/src/main/java/au/com/taaffe/spotifystreamer/service/PlayerService.java
+++ app/src/main/java/au/com/taaffe/spotifystreamer/service/PlayerService.java
@@ -1,5 +1,6 @@
 package au.com.taaffe.spotifystreamer.service;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,50 +9,212 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.session.MediaController;
 import android.media.session.MediaSession;
 import android.media.session.MediaSessionManager;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import au.com.taaffe.spotifystreamer.ParcelableTrack;
-import au.com.taaffe.spotifystreamer.PlayerActivity;
-import au.com.taaffe.spotifystreamer.R;
 
 /**
  * Created by eamon on 30/07/15.
  */
 public class PlayerService extends Service {
+
     private static final String LOG_TAG = PlayerService.class.getSimpleName();
-
-    public static final String PLAYLIST = "track_list";
-    public static final String TRACK_INDEX = "track_index";
-
-    private static final int PLAYER_NOTIFICATION_ID = 876;
     private static final String MEDIA_SESSION_TAG = "PST";
+    private static final int INVALID_TRACK_ID = -1;
+    private static final int NOTIFICATION_ID = 3121;
+
+    public static final String ACTION_PLAY = "action_play";
+    public static final String ACTION_PAUSE = "action_pause";
+    public static final String ACTION_NEXT = "action_next";
+    public static final String ACTION_PREVIOUS = "action_previous";
+    public static final String ACTION_STOP = "action_stop";
+    public static final String EXTRA_PLAYLIST = "track_list";
+    public static final String EXTRA_TRACK_ID = "extra_track_id";
 
     private final IBinder mBinder = new PlayerBinder();
 
-    private ArrayList<ParcelableTrack> mPlaylist;
-    private int mTrackIndex = -1;
     private MediaPlayer mMediaPlayer;
+    private MediaSessionManager mManager;
+    private MediaSession mSession;
+    private MediaController mController;
+    private Bitmap mAlbumImage;
+    private ArrayList<ParcelableTrack> mPlaylist;
+    private int mTrackId = INVALID_TRACK_ID;
     private PlayerServiceListener mListener;
-    private Bitmap mAlbumBitmap;
-    private NotificationManager mNotificationManager;
-    private MediaSessionManager mMediaManager;
-    private MediaSession mMediaSession;
+
+    private Target mTarget = new Target() {
+        @Override
+        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+            Log.v(LOG_TAG,"onBitmapLoaded");
+            mAlbumImage = bitmap;
+            createNotification();
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable errorDrawable) {
+            Log.d(LOG_TAG,"Image load failed");
+        }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mManager == null) {
+            initMediaSession();
+        }
+        if (intent != null && intent.hasExtra(EXTRA_PLAYLIST)) {
+            mPlaylist = intent.getParcelableArrayListExtra(EXTRA_PLAYLIST);
+
+            // When a playlist is recieved start from the beginning unless a trackId has
+            // been specified
+            mTrackId = 0;
+        }
+        if (intent != null && intent.hasExtra(EXTRA_TRACK_ID)) {
+            mTrackId = intent.getIntExtra(EXTRA_TRACK_ID, INVALID_TRACK_ID);
+        }
+
+        if (intent != null && intent.getAction() != null) {
+            handleAction(intent.getAction());
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @TargetApi(21)
+    public void handleAction(String action) {
+
+        switch (action) {
+            case ACTION_PLAY:
+                Log.v(LOG_TAG, "ACTION_PLAY");
+                mController.getTransportControls().play();
+                break;
+            case ACTION_PREVIOUS:
+                Log.v(LOG_TAG,"ACTION_PREVIOUS");
+                mController.getTransportControls().skipToPrevious();
+                break;
+            case ACTION_PAUSE:
+                Log.v(LOG_TAG, "ACTION_PAUSE");
+                mController.getTransportControls().pause();
+                break;
+            case ACTION_NEXT:
+                Log.v(LOG_TAG,"ACTION_NEXT");
+                mController.getTransportControls().skipToNext();
+                break;
+            default:
+                return;
+        }
+    }
+
+    public void loadTrack() {
+        Log.v(LOG_TAG,"loadTrack");
+        if(mTrackId == INVALID_TRACK_ID)
+            return;
+
+        Picasso.with(this).load(mPlaylist.get(mTrackId).track_image_url).into(mTarget);
+    }
+
+    @TargetApi(21)
+    public void initMediaSession() {
+        mMediaPlayer = new MediaPlayer();
+        mManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
+        mSession = new MediaSession(this,MEDIA_SESSION_TAG);
+        mController = mSession.getController();
+
+        mSession.setCallback(new MediaSession.Callback() {
+            private final String LOG_TAG = MediaSession.Callback.class.getSimpleName();
+
+            @Override
+            public void onPlay() {
+                //TODO onPlay
+                Log.v(LOG_TAG, "onPlay");
+                loadTrack();
+            }
+
+            @Override
+            public void onPause() {
+                //TODO onPause
+                Log.v(LOG_TAG, "onPlause");
+                loadTrack();
+            }
+
+            @Override
+            public void onSkipToNext() {
+                //TODO onSkipToNext
+                mTrackId ++;
+                loadTrack();
+                Log.v(LOG_TAG, "onSkipToNext, mTrackId: " + mTrackId);
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                //TODO onSkipToPrevious
+                mTrackId--;
+                loadTrack();
+                Log.v(LOG_TAG, "onSkipToNext, mTrackId: " + mTrackId);
+            }
+        });
+
+        mSession.setActive(true);
+
+        // Indicate you want to receive transport controls via your Callback
+        mSession.setFlags(MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
+    }
+
+    @TargetApi(21)
+    private void createNotification() {
+        Log.v(LOG_TAG, "createNotification");
+        final Notification noti = new Notification.Builder(this)
+            // Hide the timestamp
+                .setShowWhen(false)
+                    // Set the Notification style
+                .setStyle(new Notification.MediaStyle()
+                        // Attach our MediaSession token
+                        .setMediaSession(mSession.getSessionToken())
+                                // Show our playback controls in the compat view
+                        .setShowActionsInCompactView(0, 1, 2))
+                    // Set the Notification color
+//                .setColor(0xFFDB4437)
+                    // Set the large and small icons
+                .setLargeIcon(mAlbumImage)
+                .setSmallIcon(android.R.drawable.stat_notify_more)
+                    // Set Notification content information
+                .setContentText(getArtist())
+                .setContentInfo(getAlbum())
+                .setContentTitle(getTrack())
+                    // Add some playback controls
+                .addAction(android.R.drawable.ic_media_previous, "prev", intentBuilder(ACTION_PREVIOUS))
+                .addAction(android.R.drawable.ic_media_pause, "pause", intentBuilder(ACTION_PAUSE))
+                .addAction(android.R.drawable.ic_media_next, "next", intentBuilder(ACTION_NEXT))
+                .build();
+
+            ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(NOTIFICATION_ID, noti);
+    }
+
+    private PendingIntent intentBuilder(String action) {
+        Intent intent = new Intent(this,PlayerService.class);
+
+        intent.setAction(action);
+
+        PendingIntent pendingIntent = PendingIntent
+                .getService(this, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+
+        return pendingIntent;
+    }
 
     public class PlayerBinder extends Binder {
         public PlayerService getService() {
@@ -62,8 +225,6 @@ public class PlayerService extends Service {
             mListener = listener;
         }
     }
-
-
 
     // Callback for views to implement
     public interface PlayerServiceListener {
@@ -82,209 +243,15 @@ public class PlayerService extends Service {
         return mBinder;
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.v(LOG_TAG, "onHandleIntent");
-
-        Bundle extras = intent.getExtras();
-        int trackIndex = -1;
-
-        if (extras == null || !extras.containsKey(PLAYLIST)) {
-            // if this information was not supplied then the service has nothing to do
-            Log.d(LOG_TAG,"Did not recieve a playlist, cannot initiate onPlay");
-            return Service.START_NOT_STICKY;
-        }
-
-        mPlaylist = extras.getParcelableArrayList(PLAYLIST);
-
-        if (!extras.containsKey(TRACK_INDEX)) {
-            mTrackIndex = 0;
-        } else {
-            mTrackIndex = extras.getInt(TRACK_INDEX);
-        }
-
-        startPlayer();
-
-        mMediaManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
-
-        return super.onStartCommand(intent, flags, startId);
+    public String getArtist() {
+        return mPlaylist.get(mTrackId).artist;
     }
-
-
-    private void showPlayerNotification() {
-        // NotificationCompatBuilder is a very convenient way to build backward-compatible
-        // notifications.  Just throw in some data.
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setColor(getResources().getColor(R.color.primary))
-                        .setSmallIcon(R.mipmap.ic_spotify_launcher)
-                        .setContentTitle("PlayerTitle")
-                        .setContentText("PlayerContent");
-
-        // Make something interesting happen when the user clicks on the notification.
-        // In this case, opening the app is sufficient.
-        Intent resultIntent = new Intent(this, PlayerActivity.class);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        builder.setContentIntent(resultPendingIntent);
-
-        mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // WEATHER_NOTIFICATION_ID allows you to update the notification later on.
-        mNotificationManager.notify(PLAYER_NOTIFICATION_ID, builder.build());
+    public String getAlbum() {
+        return mPlaylist.get(mTrackId).album;
     }
-
-    private void startPlayer(){
-        Log.v(LOG_TAG, "initialisePlayer");
-
-        mMediaPlayer = new MediaPlayer();
-
-        // Since the preparation is asynchronous we need to listen for error so that they can
-        // be handled rather than crashing the application.
-        mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mp, int what, int extra) {
-                Log.e(LOG_TAG, String.format("Error(%s%s)", what, extra));
-                return false;
-            }
-        });
-
-        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                nextTrack();
-            }
-        });
-
-        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mMediaPlayer.start();
-                if(mListener != null) {
-                    mListener.updateStatus();
-                }
-            }
-        });
-
-        playTrack(mTrackIndex);
+    public String getTrack() {
+        Log.v(LOG_TAG,"getTrack: " + mPlaylist.get(mTrackId).track_name);
+        return mPlaylist.get(mTrackId).track_name;
     }
-
-    private void nextTrack() {
-        if (mPlaylist.size() > mTrackIndex + 1) {
-            mMediaPlayer.reset();
-            if(mListener != null) {
-                mListener.updateStatus();
-            }
-            playTrack(mTrackIndex ++);
-        } else {
-            Toast.makeText(this,"This is the last track!",Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void previousTrack(){
-        if (mTrackIndex - 1 >= 0) {
-            mMediaPlayer.reset();
-            playTrack(mTrackIndex --);
-        } else {
-            Toast.makeText(this,"This is the first track!",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void playTrack(int trackIndex) {
-        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        Log.v(LOG_TAG,"trackIndex is: " + trackIndex);
-        Log.v(LOG_TAG,"mPlaylist.size(): " + mPlaylist.size());
-        String previewUrl = mPlaylist.get(mTrackIndex).track_preview_url;
-
-        try {
-            mMediaPlayer.setDataSource(previewUrl);
-            mMediaPlayer.prepareAsync();
-        } catch (IllegalArgumentException e) {
-            Log.e(LOG_TAG, e.getMessage());
-        } catch (IOException e) {
-            Log.e(LOG_TAG, e.getMessage());
-        }
-
-        // if a view is listening tell it to update itself
-        if (mListener != null) {
-            mListener.updateTrack();
-        }
-
-        // load the album image
-        Picasso.with(this).load(mPlaylist.get(mTrackIndex).track_image_url).into(new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                mAlbumBitmap = bitmap;
-            }
-
-            @Override
-            public void onBitmapFailed(Drawable errorDrawable) {
-                Log.d(LOG_TAG,"Image load failed");
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-
-            }
-        });
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mNotificationManager.cancel(PLAYER_NOTIFICATION_ID);
-    }
-// Public methods to interact with views
-
-    public void onPlay(){
-        if(mMediaPlayer != null) {
-            mMediaPlayer.start();
-            if(mListener != null) {
-                mListener.updateStatus();
-            }
-        }
-    }
-    public void onPause(){
-        if (mMediaPlayer != null) {
-            mMediaPlayer.pause();
-            if(mListener != null) {
-                mListener.updateStatus();
-            }
-        }
-    }
-    public void onNext(){
-        nextTrack();
-    }
-    public void onPrevious() {
-        previousTrack();
-    }
-    public boolean isPlaying(){
-        if(mMediaPlayer != null) {
-            return mMediaPlayer.isPlaying();
-        }
-        return false;
-    }
-    public String getArtistText(){
-        return mPlaylist.get(mTrackIndex).artist;
-    }
-
-    public String getAlbumText(){
-        return mPlaylist.get(mTrackIndex).album;
-    }
-    public String getTrackText(){
-        return mPlaylist.get(mTrackIndex).track_name;
-    }
-    public String getAlbumImageUrl(){
-        return mPlaylist.get(mTrackIndex).track_image_url;
-    }
+    public Bitmap getAlbumImage() { return mAlbumImage; }
 }
